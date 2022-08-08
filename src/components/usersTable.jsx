c6618bfc5025e0be85fb1829ad8c6aa5ffd659ed
Татьяна Вырупаева <volkova_ta_job@mail.ru>
2022-08-08 14:52:38 +1000

    Fix favorites sort

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -24,7 +24,7 @@ const UsersTable = ({
         },
         rate: { path: "rate", name: "Оценка" },
         bookmark: {
-            path: "bookmark",
+            path: "favorites",
             name: "Избранное",
             component: (user) => (
                 <Bookmark
