0c848112af893f4cd9b6a6c237c66e603e2cdb20
Татьяна Вырупаева <volkova_ta_job@mail.ru>
2022-09-15 15:56:39 +1000

    User info cards created

--- src/components/page/userPage/userPage.jsx
+++ src/components/page/userPage/userPage.jsx
@@ -1,40 +1,38 @@
 import React, { useEffect, useState } from "react";
 import api from "../../../api";
-import { useHistory } from "react-router-dom";
-import QualitiesList from "../../ui/qualities/qualitiesList";
+import UserCard from "../../ui/userInfo/userCard";
 import PropTypes from "prop-types";
+import QualitiesCard from "../../ui/userInfo/qualitiesCard";
+import MeetingsCard from "../../ui/userInfo/meetingsCard";
+import CommentsList from "../../ui/comments/commentsList";
 
 const UserPage = ({ userId }) => {
     const [user, setUser] = useState();
 
-    const history = useHistory();
-
     useEffect(() => {
         api.users.getById(userId).then((user) => {
             setUser(user);
-            console.log("user: ", user);
         });
     }, []);
-    const handleMoveToEdit = () => {
-        history.push(`/users/${userId}/edit`);
-    };
     if (!user) {
         return <h1>Loading</h1>;
     }
     return (
-        <>
-            <h1>{user.name}</h1>
-            <h2>Профессия: {user.profession.name}</h2>
-            <QualitiesList qualities={user.qualities} />
-            <h3>CompletedMeetings: {user.completedMeetings}</h3>
-            <h2>Rate: {user.rate}</h2>
-            <button onClick={handleMoveToEdit}>Редактировать</button>
-        </>
+        <div className="container">
+            <div className="row gutters-sm">
+                <div className="col-md-4 mb-3">
+                    <UserCard user={user} />
+                    <QualitiesCard qualities={user.qualities} />
+                    <MeetingsCard meetingsCount={user.completedMeetings} />
+                </div>
+                <div className="col-md-8">
+                    <CommentsList userId={userId} />
+                </div>
+            </div>
+        </div>
     );
 };
 
-UserPage.propTypes = {
-    userId: PropTypes.string.isRequired
-};
+UserPage.propTypes = { userId: PropTypes.string.isRequired };
 
 export default UserPage;
